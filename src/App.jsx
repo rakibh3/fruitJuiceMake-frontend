173850377a99aff4fdc8bd409bbf2a6c2c6a227b
Md Rakibul Hasan <hello.rakibh@gmail.com>
2024-08-29 19:59:17 +0600

    Setup prettier-plugin-tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 function App() {
   return (
     <>
-      <h1 className="text-4xl text-center text-teal-600 mt-10">Hello World</h1>
+      <h1 className="mt-10 text-center text-4xl font-bold text-teal-600">
+        Hello World
+      </h1>
     </>
   );
 }
