08bce72695d0effea9eebedcff6ba3ca342ba18f
Md Rakibul Hasan <hello.rakibh@gmail.com>
2024-10-08 11:51:33 +0600

    fix(coin-transfer): correct axios post request structure for token authorization and data handling

--- src/hooks/useCoinTransfer.jsx
+++ src/hooks/useCoinTransfer.jsx
@@ -1,16 +1,16 @@
 import { useState } from 'react'
-import axios from 'axios'
+import usePublicAxios from './useAxiosPublic'
 import { handleError } from '@/error/errorHandler'
 
 export const useCoinTransfer = () => {
   const [isLoading, setIsLoading] = useState(false)
+  const publicxios = usePublicAxios()
 
   const transferCoins = async (recipeId) => {
     setIsLoading(true)
     try {
       const token = localStorage.getItem('accessToken')
-
-      const response = await axios.post(
+      const response = await publicxios.post(
         '/coin/transfer',
         { recipeId },
         {
