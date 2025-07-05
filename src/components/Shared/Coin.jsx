e7b7f8f2a5a923a48c24bc216da45358ecbe15c9
Md Rakibul Hasan <hello.rakibh@gmail.com>
2025-07-06 04:07:24 +0600

    fix: prevent error when coin data is null

--- src/components/Shared/Coin.jsx
+++ src/components/Shared/Coin.jsx
@@ -9,7 +9,7 @@ const Coin = () => {
     <>
       <div className="flex items-center gap-2 rounded-2xl border-2 border-rose-500 px-[6px] py-[6px] dark:border-rose-400">
         <BsCoin className="h-5 w-5 text-yellow-500" />
-        <span className="font-bold dark:text-white">{coins?.data.coin}</span>
+        <span className="font-bold dark:text-white">{coins?.data?.coin}</span>
       </div>
     </>
   )
